6ad5a9241c220ab107c7a03b8ac6b232613a06ca
Valeria Locurcio <v.locurcio@hotmail.com>
2026-01-14 12:48:19 +0100

    Add files

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main (String[] args){
+        Punto punto1 = new Punto (5.5,6);
+        Punto punto2 = new Punto (4.3,8.9);
+
+        System.out.println("Primo punto= " + punto1);
+        System.out.println("Secondo punto= " + punto2);
+    }
+}
